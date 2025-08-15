3f06a8e4a3aca4ef612487681347f994c64ad1d3
Manas Parasar <manasphx@gmail.com>
2025-08-14 18:37:40 -0700

    Testing

---
+++ .history/tailwind.config_20250814182545.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+<<<<<<< HEAD
+        'custom-gray': '#555',
+=======
+        'nhs-blue-dark': '#005EB8',
+        'nhs-blue-light': '#E0F2F7',
+>>>>>>> ab05603 (Restored partially desigend dashboard)
+      },
+    },
+  },
+  plugins: [],
+}
